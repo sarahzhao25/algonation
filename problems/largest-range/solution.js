a2793b3635fc2d0e0e9e8dfe0498175a908425e5
Sarah Zhao <sarahzhao25@gmail.com>
2018-05-14 15:04:21 -0400

    added O n solution

--- problems/largest-range/solution.js
+++ problems/largest-range/solution.js
@@ -19,3 +19,31 @@ function largestRange(array) {
 }
 
 //nlogn time to sort the array, and O(n^2) time worst case
+
+function largestRange(array) {
+  const hashT = {};
+  let largestRange = [0, 0];
+  //run through the array and map the values as hashT keys & true as values
+  for (let i = 0; i < array.length; i++) {
+    hashT[array[i]] = true;
+  }
+
+  //run through the array one more time
+  for (let i = 0; i < array.length; i++) {
+    //hunt for the left and right sides of the value
+    let currVal = array[i];
+    let left = currVal, right = currVal + 1;
+    while (hashT[left]) {
+      hashT[left] = false;
+      left--;
+    }
+    while (hashT[right]) {
+      hashT[right] = false;
+      right++;
+    }
+    if (largestRange[1] - largestRange[0] <= (right - 1) - (left + 1)) largestRange = [+left + 1, +right - 1];
+  }
+  return largestRange;
+}
+
+//O(n) time to run through the array
